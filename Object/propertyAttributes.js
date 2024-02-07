296d5ae84410f3673dfbfe3494bcbc6d5f0c7dc1
samAd0san <sohaibsamad.hala@gmail.com>
2024-02-07 18:00:47 +0530

    Property Attributes

--- Object/propertyAttributes.js
+++ Object/propertyAttributes.js
@@ -6,13 +6,29 @@ var m1 = Object.create({}, {
     width: {value: 5, writable: false, enumerable: true, configurable: false}
 });
 
-/* Writable: false ensures that attempting to change the value of m1.width
+/* 1. Writable: false ensures that attempting to change the value of m1.width
 will have no effect */
 m1.width = 10;
 console.log(m1.width);
 
-// with configurable: false we cannot perform any operations with the object
+// 2. With configurable: false we cannot perform any operations with the object
 console.log(delete m1.width); // false because configurable is false
 
-// To explain enumeration i will demonstrate with the help of an example
+// 3. To explain enumeration i will demonstrate with the help of an example
 
+var student = {
+    id: 118,
+    name: 'Sam'
+};
+
+// Now by using Object.DefineProperty(); i am adding new keyValue pair
+
+Object.defineProperty(student,'Dept',{
+    enumerable: false, // On making enumerable false we cannot add a new keyValue pair to the object 
+    writable: false, // making writable false will not allow me to modify the value
+    value: 'IT'
+});
+
+// student.Dept = 'CSE'; // This value cannot be modified because writable is false
+
+console.log(student);
