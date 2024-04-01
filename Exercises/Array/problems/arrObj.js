7e33183706b4dad0994b63d2e4f549c693add0fb
samAd0san <sohaibsamad.hala@gmail.com>
2024-04-01 23:55:52 +0530

    sort array by key

---
+++ Exercises/Array/problems/arrObj.js
@@ -0,0 +1,15 @@
+// Write a function that takes an array of objects and a key, and returns a new array sorted based on the values of that key in ascending order. 
+
+function sortByKey(arr,key){
+    arr.sort((a,b) => a[key] - b[key]);
+    return arr;
+}
+
+const data = [
+    {id : 1, name : 'samuel', age : 20},
+    {id : 2, name : 'samkan', age : 35},
+    {id : 3, name : 'hahoo', age : 30},
+];
+
+const result = sortByKey(data,'age');
+console.log(result);
