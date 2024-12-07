64ccdaf6f3fcc516c5103e2746dd628d5d3e0cdd
Sohaib samad <115487764+samAd0san@users.noreply.github.com>
2024-12-07 23:51:28 +0530

    Generate Password

--- temp/exercise.js
+++ temp/exercise.js
@@ -78,3 +78,19 @@ const arrSum = (arr) => {
 
 console.log(arrSum([1,2,3,4,5]));
 console.log(arrSum([5,5]));
+
+function generatePassword(length) {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+
+  return password;
+}
+
+const passwordLength = 12;
+const randomPassword = generatePassword(passwordLength);
+console.log(randomPassword);
